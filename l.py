7134fe0694b0bbf297abcd1f72b7ff21a691b11d
khane28e <khane28e@gmail.com>
2025-11-20 21:27:45 +0800

    Create l.py

---
+++ l.py
@@ -0,0 +1,15 @@
+# name = input("Enter Username")
+
+# while name == "":
+#  print("invalid")
+#  name = input("Enter Username")
+
+# print(f"hello {name}")
+
+# age = int(input("enter your age:"))
+
+# while age == "" or not int(age) < 0:
+  print("age cannot be negative")
+  age = int(input("enter your age:"))
+
+# print(f"you are {age} years old!")
